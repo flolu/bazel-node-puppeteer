f46c648b33cc18d9d6682942346e2b69779a53a8
flolu <loflude@gmail.com>
2021-09-23 20:55:06 +0200

    setup repository

---
+++ README.md
@@ -0,0 +1,17 @@
+## Requirements
+
+- Linux
+- Yarn
+- Node.js
+
+## Setup
+
+- `yarn install`
+
+## Usage
+
+- `yarn bazelisk run //:server`
+- Expected output
+  ```
+  <Buffer 89 50 4e 47 0d 0a 1a 0a 00 00 00 0d 49 48 44 52 00 00 00 c8 00 00 00 64 08 06 00 00 00 c3 86 7f 0b 00 00 00 01 73 52 47 42 00 ae ce 1c e9 00 00 04 2b ... 1087 more bytes>
+  ```
---
+++ index.ts
@@ -0,0 +1,36 @@
+import puppeteer from 'puppeteer'
+
+async function main() {
+  const url = "https://www.w3schools.com/html/html5_canvas.asp";
+
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+    ],
+    headless: true,
+    dumpio: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  const base64Image = await page.evaluate(() => {
+    const canvas = document.querySelector("#myCanvas6");
+    return (canvas as any).toDataURL();
+  });
+
+  const base64Data = base64Image.replace("data:image/png;base64,", "");
+  const buffer = Buffer.from(base64Data, "base64");
+
+  console.log(buffer);
+
+  await browser.close();
+
+  process.exit(0);
+}
+
+main();
